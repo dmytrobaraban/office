24c875bd0aec0d08d8120780fc873b513e52dfdc
dmytrobaraban <dmitrijbaraban@gmail.com>
2021-08-27 16:51:14 +0300

    check this Illya Kanatov

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var el = document.getElementById('myDiv');
 function show() {
 document.getElementById('myDiv').classList.toggle("show");
 if(el.classList.contains('show'))
-{     //alert(document.getElementById('btn').classList.contains('show'));
+{     
      document.getElementById('btn').innerHTML='Show';
 }
 else
@@ -14,11 +14,11 @@ else
 }
 }
 
-document.getElementById('btn2').onclick=falling;
+document.getElementById('btn2').onclick=createElement;
 
-function falling()
+function createElement()
 {
-var el2 = document.createElement('span');
+let el2 = document.createElement('div');
 el2.setAttribute("class" , "fuck");
 el2.setAttribute("id" , 'you');
 // el2.innerHTML='Ohohoh';
@@ -27,8 +27,14 @@ return;
 console.log(el2);
 }
 
+document.getElementById('btn3').onclick=deleteElement;
+
+function deleteElement(){
+document.getElementById('you').remove();
+}
+
 let str = "<table class='tabs'>";
-for (let i = 1; i < 6; i++) {
+for (let i = 1; i < 5; i++) {
   str += "<tr>";
   str += `<td class="tab">${i}</td>`;
   str += `<td class="tab">${i * i}</td>`;
